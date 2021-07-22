01b22d4c0335b84e08b85a1871ee93ba817b910d
kenneth-Oghweniale <oghwenialekenneth@gmail.com>
2021-07-22 16:33:55 +0100

    navigation component works so far

--- app/src/main/java/com/timothy/myfragment/AFragment.kt
+++ app/src/main/java/com/timothy/myfragment/AFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.navigation.fragment.findNavController
 import com.timothy.myfragment.databinding.FragmentABinding
 
 class AFragment : Fragment() {
@@ -19,16 +20,16 @@ class AFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentABinding.inflate(inflater, container, false)
-        return inflater.inflate(R.layout.fragment_a, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.moveBtn.setOnClickListener {
-           val activity = requireActivity() as MainActivity
-            activity.navigate(BFragment())
+            findNavController().navigate(R.id.action_AFragment_to_BFragment)
         }
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onDestroyView() {
